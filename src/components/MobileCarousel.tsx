86ed6eabed9e38ccae6ae3b9880257e79ab64597
Tyler Eads <tyler.eads9@gmail.com>
2020-05-12 19:09:00 -0400

    Added Mobile BreakPoint Improvements

--- src/components/MobileCarousel.tsx
+++ src/components/MobileCarousel.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { PRODUCT_CARDS, PULSE_DOTS } from '../data';
+import { ProductCard } from './ProductCard';
+import { Pulser } from './Pulser';
 
 export const MobileCarousel = () => {
+    const [card, setCard]: any = useState({
+        spotlightLeftZone: false,
+        tvSpeakerZone: false,
+        tvDeviceZone: false,
+        tvThermostatZone: false,
+        bedroomLightZone: false,
+        bedroomTableZone: false,
+        spotlightRightZone: false,
+        outsideMotionSensorLeft: false,
+        kitchenTvZone: false,
+        doorKeypadZone: false,
+        doorLockZone: false,
+        garageWaterTankZone: false,
+        garageScreenZone: false,
+        outsideMotionSensorRight: false,
+    });
+
+    const toggle = (id: string) => {
+        const clonedCardState = Object.assign({}, card);
+        for (let c in clonedCardState) {
+            c === id ? (card[c] = card[c]) : (card[c] = false);
+        }
+        setCard({ ...card, ...clonedCardState });
+        setCard({ ...card, [id]: !card[id] });
+    };
+
     return (
         <Carousel
-            autoPlay={true}
+            // autoPlay={true}
             showThumbs={false}
             showArrows={false}
             showStatus={false}
@@ -14,6 +43,54 @@ export const MobileCarousel = () => {
         >
             <div className="adi-mobile-image adi-tv-room">
                 {/* TV Room Pulsers & Cards */}
+                <ul className="adi--card-list">
+                    {PRODUCT_CARDS.map((c) => {
+                        const {
+                            id,
+                            arrowDirection,
+                            productImage,
+                            productImageAltText,
+                            productMetaLink,
+                            copy,
+                            productMetaLinkText,
+                            productMetaName,
+                            shopMoreBtn,
+                            shopMoreBtnLink,
+                            title,
+                        } = c;
+                        return (
+                            <li id={`${id}Card`} key={id} className="p-abs">
+                                <ProductCard
+                                    isOpen={card[id]}
+                                    arrowDirection={arrowDirection}
+                                    productImage={productImage}
+                                    productImageAltText={productImageAltText}
+                                    productMetaLink={productMetaLink}
+                                    onClose={() => toggle(id)}
+                                    copy={copy}
+                                    productMetaLinkText={productMetaLinkText}
+                                    productMetaName={productMetaName}
+                                    shopMoreBtn={shopMoreBtn}
+                                    shopMoreBtnLink={shopMoreBtnLink}
+                                    title={title}
+                                ></ProductCard>
+                            </li>
+                        );
+                    })}
+
+                    {PULSE_DOTS.map((x) => {
+                        return (
+                            <li
+                                className="pulser"
+                                key={x}
+                                id={x}
+                                onClick={() => toggle(x)}
+                            >
+                                <Pulser />
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
             <div className="adi-mobile-image">
                 {/* Bedroom Pulsers & Cards */}
--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ if (typeof Object.assign !== 'function') {
     // Must be writable: true, enumerable: false, configurable: true
     Object.defineProperty(Object, 'assign', {
         value: function assign(target: any, varArgs: any) {
-            // .length of function is 2
-            'use strict';
             if (target === null || target === undefined) {
                 throw new TypeError(
                     'Cannot convert undefined or null to object'
